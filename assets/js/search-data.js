50c134a5b3f6200aebc87d9970e920cea52cfd97
singhnishit <f20221317@pilani.bits-pilani.ac.in>
2025-12-20 12:19:04 +0000

    Deploying to gh-pages from @ singhnishit/singhnishit.github.io@fe690e55ecbdbcc8443da4a0b7504e5484e1ec75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,11 +437,6 @@ ninja.data = [{
           description: "Implementing a CycleGAN",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
