6275903ab8b1acc20384f41d406d85aaa0564731
singhnishit <f20221317@pilani.bits-pilani.ac.in>
2025-12-20 12:21:30 +0000

    Deploying to gh-pages from @ singhnishit/singhnishit.github.io@13da1e30fe0e9e3207383dab90a056a0fd9eb961 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/blog/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Excuse the incompleteness of almost all of these. The files could not be uploaded to GitHub. These are the skeletal versions.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
